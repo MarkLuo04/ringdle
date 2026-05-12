05c46f277a5fcbcfb0902cc29125194593cd6a37
Mark Luo <169178394+MarkLuo04@users.noreply.github.com>
2026-05-12 14:58:06 -0400

    build: update to prisma 7.8.0

--- ringdle/src/server/db.ts
+++ ringdle/src/server/db.ts
@@ -1,11 +1,15 @@
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { env } from "~/env";
 import { PrismaClient } from "../../generated/prisma";
 
-const createPrismaClient = () =>
-  new PrismaClient({
+const createPrismaClient = () => {
+  const adapter = new PrismaLibSql({ url: env.DATABASE_URL });
+  return new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
+};
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
